2d3dcc5935396924ddd2eff2eab50694abb755c9
Bisa Agung Saputro <bisaagungsaputro@gmail.com>
2022-10-29 19:32:07 +0700

    Perbarui Dart Basic & Referensi Belajar

--- README.md
+++ README.md
@@ -1,5 +1,40 @@
 # flutter_fugi_intensif_club_bisa_agung
 
+Referensi Saya Belajar Via Website Dart & Flutter :
+
+1. Website Resmi Flutter Langsung Dokumentasinya : https://docs.flutter.dev/
+
+2. Belajar Dart : https://www.tutorialspoint.com/dart_programming/index.htm
+
+---
+
+Referensi Saya Belajar Via Youtube Dart & Flutter :
+
+1. Youtube Programmer Zaman Now : https://www.youtube.com/playlist?list=PL-CtdCApEFH9qVH17yCbWZICYopHj4vhn
+
+2. Youtube Erico Darmawan Handayo : https://www.youtube.com/c/EricoDarmawanHandoyo/playlists
+
+3. Youtube Kuldi Project : https://www.youtube.com/c/KuldiiProject/playlists
+
+4. Youtube Code With Bahri : https://www.youtube.com/c/SaifulBahri27/playlists
+
+5. Youtube Capek Ngoding : https://www.youtube.com/c/CapekNgoding/playlists
+
+6. Youtube Dengan Bahasa English : https://www.youtube.com/c/VandadNP/playlists
+
+7. Ketik Aja di Youtube Tutorial Flutter, Akan Muncul Orang Indonesia Dan Internasional
+
+---
+
+Referensi Saya Belajar Course Online Berbayar Dart & Flutter :
+
+1. Dicoding Bisa Dapat Beasiswa Gratis Tunggu Update Info Instagram Dicoding : https://www.dicoding.com/academies/list
+- Instagram Dicoding : https://www.instagram.com/dicoding/
+
+2. Udemy Course Internasional Dengan Bahasa English : https://www.udemy.com/
+
+3. Belajar Coding Dan Bonus Belajar Design : https://www.buildwithangga.com/
+
 A new Flutter project.
 
 ## Getting Started
--- fugi/tugas_introduction/dart_dasar.dart
+++ fugi/tugas_introduction/dart_dasar.dart
@@ -1,4 +1,4 @@
-main () {
+main() {
   // Komentar 1 Baris => Fungsi Inti Codingan
   /*
     Komentar Multi Line => Memberikan Sebuah Informasi Setiap Fungsi Pada Sebuah Codingan
@@ -8,7 +8,7 @@ main () {
   */
   /// Komentar Untuk Dokumentasi Pada Sebuah Program
 
-  // Variable Adalah Wadah Untuk Menyimpan Data
+  // Variable Adalah Wadah Untuk Menyimpan Data, Bisa Memanggil Data Berulang2 Kali
 
   // Variable int => Rumus Luas Segitiga
   var alas = 10;
@@ -27,8 +27,14 @@ main () {
   // Variable String
   var nama = "Bisa Agung";
 
+  var names = "Vira Agistasari"; // Data Lama Tidak Dipoanggil
+  names = "Aisyah Citra Adini"; // Hanya Data Baru Yang Di Tampilin / Nimpa Data
+
   print("Variable String");
-  print(nama);
+  print(nama); // Bisa Memanggil Berulang2 Kali - Panggil 1
+  print(nama); // Panggil 2
+
+  print(names);
 
   // Variable Boolean
   var iniTrue = true;
@@ -56,9 +62,18 @@ main () {
   // Warning Tipe Data String Tidak Bisa Menggunakan Rumus Aritmatika
   String mylove = "Atika Nur Aini";
 
+  String ojoNangis = "Jangan Nangis"; // Data Lama Tidak Ditampilin
+
+  ojoNangis =
+      "Siap Tidak Nangis Lagi"; // Nimpa Data / Hanya Ditampilin Data Baru Aja
+
+  print(ojoNangis);
+
   print("Tipe Data String");
   print("Saya Mencintaimu ${mylove}777*^%");
   print("10 + 5"); // Contoh Tipe Data String Tidak Bisa Menggunakan Aritmatika
+
+  print("Gabungan Antara String Dengan Proses Aritmatika ${10 + 5}");
 
   // Tipe Data Boolean
   //true => Mempresentasikan Kebenaran
@@ -85,4 +100,42 @@ main () {
   print("Final");
   print(suami);
   print(waktu);
+
+  // Aritmatika => Perhitungan Matematika => Bisa + Sampai Minus - (Bisa Pakai Number => int dan double) Bisa Juga Pakai var dan dynamic
+  /*
+    1. Pertambahan => +
+    2. Pengurangan => -
+    3. Perkalian => *
+    4. Pembagian => /
+    6. Sisa Bagi / Modulus => &
+  */
+
+  print("Aritmatika Pertambahan => +");
+  int pertambahanSatu = 25;
+  double pertambahanDua = 30.5;
+
+  print(pertambahanSatu + pertambahanDua);
+
+  print("Aritmatika Pengurangan => -");
+  int penguranganSatu = 20;
+  int penguranganDua = 13;
+  print(penguranganSatu - penguranganDua);
+  print(20 - 90); // Minus - 70
+
+  print("Aritmatika Perkalian => *");
+  int perkalianSatu = 10;
+  int perkalianDua = 7;
+  print(
+      "Perkalian Dari $perkalianSatu * $perkalianDua"); // Gabungan Tipe Data String & Tipe Data Number
+
+  print("Aritmatika Pembagian => /");
+  int pembagianSatu = 5000;
+  int pembagianDua = 2;
+  print(pembagianSatu / pembagianDua);
+
+  print("Aritmatika Sisa Bagi / Modulus => %");
+  int sisabagiSatu = 11;
+  int sisabagiDua = 2;
+  print(sisabagiSatu % sisabagiDua);
+
 }
